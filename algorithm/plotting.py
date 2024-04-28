561c4ee4a45841d7c8e6d6267fb18d229d0b0415
Vashisth Choudhari <f20211989@hyderabad.bits-pilani.ac.in>
2024-04-28 23:46:46 +0530

    final i think

--- algorithm/plotting.py
+++ algorithm/plotting.py
@@ -17,7 +17,7 @@ for i in content:
 def plot_rna_structure(dot_bracket, sequence):
     # Calculate the total number of nucleotides
     num_nucleotides = len(dot_bracket)
-    
+    maxh = 1
     # Scatter points in a straight line
     x = range(num_nucleotides)
     y = [0] * num_nucleotides
@@ -36,6 +36,8 @@ def plot_rna_structure(dot_bracket, sequence):
         elif char == ')':
             start_index = stack.pop()
             end_index = i
+            if end_index-start_index > maxh:
+                maxh = end_index-start_index
             arc = Arc(((start_index+end_index)/2,0), end_index-start_index, 0.05*(end_index-start_index), angle=0, theta1=0, theta2=180, color='red')
             plt.gca().add_patch(arc)
     
@@ -44,7 +46,7 @@ def plot_rna_structure(dot_bracket, sequence):
         plt.text(x_coord, -0.1, nucleotide, ha='center', va='top')
     
     plt.xlim(-1, num_nucleotides)
-    plt.ylim(-1, num_nucleotides)
+    plt.ylim(-1, 0.07*maxh)
     plt.gca().axes.get_yaxis().set_visible(False)
     plt.savefig('./static/Images/dotbrac.png')
 
