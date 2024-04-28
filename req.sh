74473cb201869053b77ce329b0f9f9a5c6e105f2
Saksham Bajaj <sakshambajaj18@gmail.com>
2024-04-28 18:13:20 +0530

    chmod x in req

--- req.sh
+++ req.sh
@@ -1,3 +1,5 @@
 sudo chmod +x ./static/shell/install_packages.sh
+sudo chmod +x ./a.out
+sudo chmod +x ./static/shell/run1.sh
 ./static/shell/install_packages.sh ./requirements/systemReqs.txt
 pip install -r ./requirements/pythonReqs.txt
